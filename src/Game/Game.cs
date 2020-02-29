4fb6ed1a835df01c216622baef4223a8f0e2c3f8
khorwl <44201963+khorwl@users.noreply.github.com>
2020-02-29 16:43:18 +0500

    permutation of two cards for third moves is added

--- src/Game/Game.cs
+++ src/Game/Game.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+using System;
+using System.Linq;
 using thegame.Game.Primitives;
 using thegame.Game.Tools;
 
@@ -8,9 +9,12 @@ namespace thegame.Game
 	{
 		public GameBoard Board { get; }
 		public GameState State { get; private set; }
+		public bool IsTranspositionStep => stepCount == 2;
+		public Card[] TranspositionCards { get; } = new Card[2];
 
 		private IPlayer player;
 		private Card openCard;
+		private int stepCount;
 
 
 		public Game()
@@ -18,7 +22,8 @@ namespace thegame.Game
 			player = null;
 			State = GameState.IN_PROCESS;
 			var generator = new BoardGenerator();
-			Board = generator.Generate(4, 4);
+			Board = generator.Generate(Constants.HeightBoard, Constants.WidthBoard);
+			stepCount = 0;
 		}
 
 		public Game(IPlayer player)
@@ -27,6 +32,7 @@ namespace thegame.Game
 			State = GameState.IN_PROCESS;
 			var generator = new BoardGenerator();
 			Board = generator.Generate(4, 4);
+			stepCount = 0;
 		}
 
 		public Card GetCard(int height, int width)
@@ -34,6 +40,9 @@ namespace thegame.Game
 			var card = Board.Cards[height, width];
 
 			CheckMatch(card);
+			stepCount = (stepCount + 1) % 3;
+			if (stepCount == 2)
+				Swap();
 
 			return card;
 		}
@@ -43,6 +52,21 @@ namespace thegame.Game
 			return GetCountOfCardsGuessed() * 100;
 		}
 
+		private void Swap()
+		{
+			var rand = new Random();
+			var y1 = rand.Next(Constants.HeightBoard - 1);
+			var x1 = rand.Next(Constants.WidthBoard - 1);
+			var y2 = rand.Next(Constants.HeightBoard - 1);
+			var x2 = rand.Next(Constants.WidthBoard - 1);
+
+			var temp = Board.Cards[y1, x1];
+			Board.Cards[y1, x1] = Board.Cards[y2, x2];
+			Board.Cards[y2, x2] = temp;
+
+			TranspositionCards[0] = Board.Cards[y1, x1];
+			TranspositionCards[1] = temp;
+		}
 
 		private void CheckMatch(Card card)
 		{
@@ -77,7 +101,7 @@ namespace thegame.Game
 		{
 			if (openCard == null)
 				return false;
-			
+
 			return openCard.Color == card.Color;
 		}
 
